679a7edf64eba1e61c9fac00803c4eb19a2bd4f8
crimsonmoin <crimsonmoin@yahoo.com>
2016-03-02 18:39:11 +0530

    video demo 1

--- js/idleTimer.js
+++ js/idleTimer.js
@@ -10,8 +10,8 @@ idleWait = 25000;
 			var str=window.location.href;
 			var res = str.split("#");
 			//alert(res[1]);
-			if(res[1]!="summarypage"&&res[1]!="mainpage"&&res[1]!="connectpage"&&res[1]!=undefined&&res[1]!="undefined")
-			{window.location.href="#mainpage";}
+			if(res[1]!="summarypage"&&res[1]!="mainpage"&&res[1]!="connectpage"&&res[1]!=undefined&&res[1]!="undefined"&&res[1]!="screensaverpage")
+			{window.location.href="#screensaverpage";}
                 idleState = true; }, idleWait);
         });
         $("body").trigger("mousemove");
