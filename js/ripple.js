3ed185fd26d3aeb9cffbab085ec2fd04d98d7e4f
crimsonmoin <crimsonmoin@yahoo.com>
2016-03-02 15:36:50 +0530

    alignment & text size cpanel

--- js/ripple.js
+++ js/ripple.js
@@ -12,16 +12,16 @@ $(function(){
 					d = Math.max($(this).outerWidth(), $(this).outerHeight());
 					ink.css({height: d, width: d});
 				}
-				$(".play").attr('src','play-icon.png');
-				$(".upload").attr('src','upload-icon.png');
+				$(".play").attr('src','img/play-icon.png');
+				$(".upload").attr('src','img/upload-icon.png');
 				$(".ripplelink").animate({backgroundColor:'transparent'}, 100).parent().animate({borderColor:'transparent'},100).parent().animate({borderColor:'transparent'},100).animate({borderColor:'transparent'},100);
 				
 				$(this).animate({backgroundColor:'#ffffff'}, 200).parent().animate({borderColor:'#ffffff'},300).parent().animate({borderColor:'#ffffff'},300).animate({borderColor:'transparent'},300);
 				if($(this).find('img').attr('title')=="play"){
-					$(this).find('img').attr('src','playh-icon.png');
+					$(this).find('img').attr('src','img/playh-icon.png');
 				}
 				else if($(this).find('img').attr('title')=="upload"){
-					$(this).find('img').attr('src','uploadh-icon.png');
+					$(this).find('img').attr('src','img/uploadh-icon.png');
 				}
 				x = e.pageX - $(this).offset().left - ink.width()/2;
 				y = e.pageY - $(this).offset().top - ink.height()/2;
