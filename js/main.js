ab400778e5206641e0b02d1c7c4de326718eff65
crimsonmoin <crimsonmoin@yahoo.com>
2016-03-03 11:13:05 +0530

    video fix 2

--- js/main.js
+++ js/main.js
@@ -35,11 +35,11 @@ $(document).on("pagecreate","#connectpage",function(){
 		}
   });    
 });
-$(document).on("pageshow","#screensaverpage",function(){
+$(document).on("pagecreate","#screensaverpage",function(){
 	var video = document.getElementById("myAudio");
 	video.loop = false; 
-    video.addEventListener('ended', function() { 
-    video.currentTime=0.1; video.play(); }, false);
+    /*video.addEventListener('ended', function() { 
+    video.currentTime=0.1; video.play(); }, false);*/
     video.play();
 });
 $(document).on("pagecreate","#mainpage",function(){
